89d0e3be51041df707e7e2bc2fa388935255e087
Andrey Lashkevich <andlashkevich@ya.ru>
2025-01-03 03:05:45 +0300

    8

--- my-app/src/task3.js
+++ my-app/src/task3.js
@@ -1,8 +1,69 @@
-import { Routes, Route, Link, useNavigate, Navigate, useParams } from 'react-router-dom';
+import { Routes, Route, useParams, Link, useNavigate, Navigate } from 'react-router-dom';
 import styles from './task.module.css';
 import { useEffect, useState, useRef } from 'react';
 
-// В task.js нет возможности зайти на существующий адрес вида/task/* из адресной строки, а только по ссылке на странице. Отключив переадресацию с несуществующих адресов /task/* на /404, всё работает. Пока не разобрался, что исправить. В task3.js тоже самое, только вывел компоненты из функции экспорта.
+const NotFnd = () => (
+	<div className={styles.notFnd}>Такой страницы не существует! Ошибка 404.</div>
+);
+
+const Main = (iT, er, cT, oT, sT, fT, o, size) => {
+	return (
+		<>
+			<div className={styles.buttons}>
+				<button disabled={!iT || er} className={styles.сButton} onClick={cT}>
+					Добавить
+				</button>
+				<button disabled={!oT} onClick={sT} className={styles.sButton}>
+					Упорядочить
+				</button>
+				<button disabled={!iT} onClick={fT} className={styles.fButton}>
+					Найти
+				</button>
+			</div>
+			<ol className={styles.ol} ref={o}>
+				{oT.map(({ task, id }) => {
+					return (
+						<Link to={`/task/${id}`} className={styles.tasks} key={id}>
+							<li className={styles.uncheck}>
+								{task.length > size ? `${task.slice(0, size)}...` : task}
+							</li>
+						</Link>
+					);
+				})}
+			</ol>
+		</>
+	);
+};
+
+const Part = (oT, rT, iT, uT, er, dT, sIT, sE) => {
+	const prm = useParams();
+	let a = oT.filter((it) => it.id === Number(prm.id));
+	return a.length ? (
+		<>
+			<div to={`/task/${a[0].id}`} className={styles.tasks}>
+				<button onClick={rT} className={styles.pButton}>
+					Назад
+				</button>
+				<button
+					disabled={!iT || er}
+					onClick={(e) => uT(e, a[0].id)}
+					className={styles.uButton}
+				>
+					Изменить
+				</button>
+				<button onClick={(e) => dT(e, a[0].id)} className={styles.del}>
+					Удалить
+				</button>
+			</div>
+			<li onClick={(e) => sIT(e.target.innerText)} className={styles.uncheck}>
+				{a[0].task}
+			</li>
+		</>
+	) : (
+		sE('')
+		// <div className={styles.notFnd}>Такой страницы не существует! Ошибка 404.</div>
+	);
+};
 
 export function App() {
 	const [inTask, setInTask] = useState('');
@@ -14,7 +75,6 @@ export function App() {
 	const setClear = () => {
 		setInTask('');
 		setError(null);
-		setRefresh(!refresh);
 	};
 	const ol = useRef(null);
 
@@ -79,7 +139,7 @@ export function App() {
 			method: 'DELETE',
 		});
 		navigate('/');
-		setClear();
+		setRefresh(!refresh);
 	};
 
 	const reTurn = () => {
@@ -87,77 +147,13 @@ export function App() {
 		navigate(-1);
 	};
 
-	const NotFnd = () => (
-		<div className={styles.notFnd}>Такой страницы не существует! Ошибка 404.</div>
-	);
-
-	const Main = () => (
-		<>
-			<div className={styles.buttons}>
-				<button
-					disabled={!inTask || error}
-					className={styles.сButton}
-					onClick={createTask}
-				>
-					Добавить
-				</button>
-				<button disabled={!outTask} onClick={sortTask} className={styles.sButton}>
-					Упорядочить
-				</button>
-				<button disabled={!inTask} onClick={findTask} className={styles.fButton}>
-					Найти
-				</button>
-			</div>
-			<ol className={styles.ol} ref={ol}>
-				{outTask.map(({ task, id }) => {
-					return (
-						<Link to={`/task/${id}`} className={styles.tasks} key={id}>
-							<li className={styles.uncheck}>
-								{task.length > sz ? `${task.slice(0, sz)}...` : task}
-							</li>
-						</Link>
-					);
-				})}
-			</ol>
-		</>
-	);
-
 	useEffect(() => {
 		if (error === '') navigate('/404', { replace: true });
 	}, [navigate, error]);
 
-	const Part = () => {
-		const prm = useParams();
-		let a = outTask.filter((it) => it.id === Number(prm.ti));
-		return a.length ? (
-			<>
-				<div to={`/task/${a[0].id}`} className={styles.tasks}>
-					<button onClick={reTurn} className={styles.pButton}>
-						Назад
-					</button>
-					<button
-						disabled={!inTask || error}
-						onClick={(e) => updTask(e, a[0].id)}
-						className={styles.uButton}
-					>
-						Изменить
-					</button>
-					<button onClick={(e) => delTask(e, a[0].id)} className={styles.del}>
-						Удалить
-					</button>
-				</div>
-				<li
-					onClick={(e) => setInTask(e.target.innerText)}
-					className={styles.uncheck}
-				>
-					{a[0].task}
-				</li>
-			</>
-		) : (
-			setError('')
-			// <div className={styles.notFnd}>Такой страницы не существует! Ошибка 404.</div>
-		);
-	};
+	const Ma = () => Main(inTask, error, createTask, outTask, sortTask, findTask, ol, sz);
+
+	const Pa = () => Part(outTask, reTurn, inTask, updTask, error, delTask, setInTask, setError);
 
 	return (
 		<div className={styles.wrap}>
@@ -179,10 +175,10 @@ export function App() {
 			</div>
 
 			<Routes>
-				<Route path="/task/:ti" element={<Part />} />
-				<Route path="/" element={<Main />} />
-				<Route path="*" element={<Navigate to="/404" replace={true} />} />
+				<Route path="/task/:id" element={<Pa />} />
+				<Route path="/" element={<Ma />} />
 				<Route path="/404" element={<NotFnd />} />
+				<Route path="*" element={<Navigate to="/404" replace={true} />} />
 			</Routes>
 		</div>
 	);
